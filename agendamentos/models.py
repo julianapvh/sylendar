2bf417866380d7378df08d32ada039b4b0bf2bca
https://github.com/julianapvh <julianacore2@gmail.com>
2023-11-08 13:11:01 -0400

    alterações para o funcionamento do banco de dados

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.models import Group
 from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
+from django.contrib.auth.models import Permission
 
 class Equipamento(models.Model):
     nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    descricao = models.CharField(max_length=100)
+    fabricante = models.CharField(max_length=50)
+    data_aquisicao = models.DateField(default=timezone.now)
+
 
 class Usuario(AbstractUser):
     nome = models.CharField(max_length=100)
@@ -44,18 +51,19 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    nome = models.CharField(max_length=100)
-    # Outros campos personalizados
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=timezone.now)
+class CustomUser(AbstractUser):
+    # Your other fields go here
 
-    objects = CustomUserManager()
+    # Update the groups field
+    groups = models.ManyToManyField(
+        Group,
+        blank=True,
+        related_name='customuser_set',  # Use a different name for the reverse relation
+    )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nome']
-
-    def __str__(self):
-        return self.nome
+    # Update the user_permissions field
+    user_permissions = models.ManyToManyField(
+        Permission,
+        blank=True,
+        related_name='customuser_set',  # Use a different name for the reverse relation
+    )

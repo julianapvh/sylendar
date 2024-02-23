f29e5a7ca7a032e49ab2d85c84ff62b3e983f949
https://github.com/julianapvh <julianacore2@gmail.com>
2024-02-23 12:53:53 -0400

    up

--- agendamentos/forms.py
+++ agendamentos/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Agendamento, Equipamento
 from .models import Usuario
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from agendamentos.models import Usuario
 
 class AgendamentoForm(forms.ModelForm):
@@ -38,25 +38,6 @@ class UserRegistrationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-        
-class SignUpForm(UserCreationForm):
-    is_admin = forms.BooleanField(label='Administrador', required=False)
-
-    class Meta:
-        model = User
-        fields = ('username', 'password1', 'password2', 'is_admin')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if self.cleaned_data['is_admin']:
-            admin_group = Group.objects.get(name='Administradores')
-            user.groups.add(admin_group)
-        else:
-            client_group = Group.objects.get(name='Clientes')
-            user.groups.add(client_group)
         if commit:
             user.save()
         return user

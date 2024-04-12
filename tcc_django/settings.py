e32b038d1deb6592f31df4a995338a6f8a9361f8
Julianapvh <julianacore2@gmail.com>
2024-04-12 12:53:56 -0400

    adicionado o sqllite

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -96,12 +96,8 @@ WSGI_APPLICATION = 'tcc_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE', 'bd_agendamentos'),
-        'USER': os.environ.get('MYSQL_USER', 'django'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD', '@siosa'),
-        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

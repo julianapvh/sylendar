0c734550aefd52b0f668359d4f877bf2f7045fc2
Julianapvh <julianacore2@gmail.com>
2024-04-15 11:56:17 -0400

    debug =  true

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'True'
 
 ALLOWED_HOSTS = ['*',]
 

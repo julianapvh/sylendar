5217c46b274cc02ec0845c61e793e10499022a34
Julianapvh <julianacore2@gmail.com>
2024-04-16 11:44:18 -0400

    # The absolute path to the directory where collectstatic will collect static files for deployment. STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # The URL to use when referring to static files (where they will be served from) STATIC_URL = '/static/'  STATICFILES_DIRS = [     os.path.join(BASE_DIR, 'agendamentos', 'static'), ]

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -139,10 +139,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'agendamentos', 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

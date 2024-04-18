18a5de750167fab8e6349e7241a8b56f2e9006bd
Julianapvh <julianacore2@gmail.com>
2024-04-18 14:21:27 -0400

    teste 2 database azure

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,23 +88,20 @@ WSGI_APPLICATION = 'tcc_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+# in your setting file, eg. settings.py
+host = os.getenv('django.mysql.database.azure.com')
+user = os.getenv('django')
+password = os.getenv('cadelinhaSam1')
+database = os.getenv('bd_agendamentos')
 
+DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'bd_agendamentos',
-        'USER': 'django',
-        'PASSWORD': 'cadelinhaSam1',
-        'HOST': 'django.mysql.database.azure.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'ssl_ca': 'https://github.com/julianapvh/tcc_django/blob/14c19e0b1c9e6e76c427bcbd38b3aada9878c42a/ssl/DigiCertGlobalRootCA.crt.pem',
-        },
-    #'database2': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        
-    },
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': database,
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': host
+    }
 }
 
 

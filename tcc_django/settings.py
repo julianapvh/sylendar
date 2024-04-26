ea6bc7e322b147e219dae7762f9fdf6160252f39
Julianapvh <70443877+julianapvh@users.noreply.github.com>
2024-04-26 19:58:42 -0400

    remoção de notificações

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -59,13 +59,11 @@ INSTALLED_APPS = [
     # apps de terceiros
     
     'rolepermissions',
-    'notifications',
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

f67a086a2fbcecdae7bc3099af580668f175fdf3
Julianapvh <70443877+julianapvh@users.noreply.github.com>
2024-04-26 22:53:01 -0400

    teste 11

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 
 # O URL para usar ao se referir aos arquivos estáticos (de onde eles serão servidos).
-STATIC_URL = 'agendamentos'
+STATIC_URL = '/agendamentos/static/'
 
 # O caminho absoluto para o diretório onde collectstatic coletará os arquivos estáticos para implantação.
 STATIC_ROOT = os.path.join(BASE_DIR, 'agendamentos', 'staticfiles')

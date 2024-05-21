d7b1401b803350184986e9db1088cd20e7649334
julianapvh <julianacore2@gmail.com>
2024-05-21 10:49:57 -0400

    gifs error

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -819,7 +819,7 @@ def profile(request):
     
     
 def custom_403_view(request):
-    return render(request, '500.html', status=500)
+    return render(request, '403.html', status=500)
     
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)

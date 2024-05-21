222532e6ff0201e7c8502fd4b283c281beae11eb
julianapvh <julianacore2@gmail.com>
2024-05-21 13:15:57 -0400

    error 500

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -827,5 +827,5 @@ def custom_404_view(request, exception):
     return render(request, "404.html", status=404)
 
 
-def custom_500_view(request, exception):
+def custom_500_view(request):
     return render(request, "500.html", status=500)

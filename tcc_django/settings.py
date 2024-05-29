63f04b5911f3cfa5db9bc34c18c6edf01be1bf76
julianapvh <julianacore2@gmail.com>
2024-05-29 08:24:55 -0400

    desfazendo algumas alterações

--- tcc_django/settings.py
+++ tcc_django/settings.py
@@ -179,8 +179,8 @@ LOGIN_REDIRECT_URL = "home"
 # Configurações de envio do e-mail
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS").lower() == "true"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = "us2.smtp.mailhostbox.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "agendamentos@sisagenda.tech"
+EMAIL_HOST_PASSWORD = "XP@%Hs$2"

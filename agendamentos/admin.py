cf9f8fffa6bb370d58d8f33d27e638d7f0bb7dc9
julianapvh <julianacore2@gmail.com>
2024-06-19 10:22:11 -0400

    teste

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -29,50 +29,8 @@ csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = UserCreationFormWithExtraFields
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (
-            "Informações pessoais",
-            {"fields": ("first_name", "last_name", "email", "telefone")},
-        ),
-        (
-            "Permissões",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        ("Datas importantes", {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "username",
-                    "password1",
-                    "password2",
-                    "first_name",
-                    "last_name",
-                    "email",
-                    "telefone",
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                ),
-            },
-        ),
-    )
+
 
 
 # Registrar os modelos no painel de administração
 admin.site.register(Equipamento)
-admin.site.register(User, CustomUserAdmin)
